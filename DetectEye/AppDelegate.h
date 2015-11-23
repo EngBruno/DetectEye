39ece3ff800786da02d42dbeef7cdbeb42c5f472
Elisa Lemos <bruno@imac-de-acessibilidade.local>
2015-11-23 18:56:19 -0300

    Initial Commit

---
+++ DetectEye/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DetectEye
+//
+//  Created by Bruno on 23/11/15.
+//  Copyright © 2015 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DetectEye/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DetectEye
+//
+//  Created by Bruno on 23/11/15.
+//  Copyright © 2015 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
